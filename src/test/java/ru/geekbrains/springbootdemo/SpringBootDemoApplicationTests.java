7d51c2783ef8dbaf9fee3e470b16395e8afbfedc
Yaroslav <beyaroslav@gmail.com>
2021-05-27 22:22:37 +0700

    Initial commit

---
+++ src/test/java/ru/geekbrains/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
