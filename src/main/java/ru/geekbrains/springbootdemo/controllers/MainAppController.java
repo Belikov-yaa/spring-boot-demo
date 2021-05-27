2036f39cb59b706a1245b51ad4c198fa3d0a4669
Yaroslav <beyaroslav@gmail.com>
2021-05-27 23:58:08 +0700

    homework almost done

--- src/main/java/ru/geekbrains/springbootdemo/controllers/MainAppController.java
+++ src/main/java/ru/geekbrains/springbootdemo/controllers/MainAppController.java
@@ -1,30 +1,36 @@
 package ru.geekbrains.springbootdemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.geekbrains.springbootdemo.enities.Product;
+import ru.geekbrains.springbootdemo.service.ProductService;
 
 import java.util.List;
 
 @Controller
 //@RequestMapping("/product")
 public class MainAppController {
+    private ProductService productsService;
+
+    @Autowired
+    public void setProductsService(ProductService productsService) {
+        this.productsService = productsService;
+    }
 
     @GetMapping("")
     public String showAllProducts(Model model) {
-//        List<Product> products = productsService.getProductsList();
-        Product product = new Product(1, "Potato", 3.40);
-        model.addAttribute("products", product);
+        List<Product> products = productsService.getProductsList();
+        model.addAttribute("products", products);
         return "index";
     }
 
     @GetMapping("/{id}")
     public String showProductById(Model model, @PathVariable(name = "id") int id) {
-//        Product product = productsService.getProductById(id);
-        Product product = new Product(id, "Potato", 3.40);
+        Product product = productsService.getProductById(id);
         model.addAttribute("product", product);
         return "product";
     }
