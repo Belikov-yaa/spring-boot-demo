cb94abb73682aaceb4b87169c8f1dfe66dd11bd9
Yaroslav <beyaroslav@gmail.com>
2021-06-21 22:48:00 +0700

    Spring security added add entities: User, Authority

---
+++ src/main/java/ru/geekbrains/springbootdemo/enities/User.java
@@ -0,0 +1,56 @@
+package ru.geekbrains.springbootdemo.enities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @OneToMany(mappedBy = "user")
+    private List<Authority> authorities;
+
+    public String getUsername() {
+        return userName;
+    }
+
+    public void setUsername(String username) {
+        this.userName = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public User() {
+    }
+}
