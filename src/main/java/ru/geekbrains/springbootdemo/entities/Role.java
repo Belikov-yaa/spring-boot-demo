0e68109de7efec90d15c69da263a9fe592ee8f67
Yaroslav <beyaroslav@gmail.com>
2021-07-06 00:49:39 +0700

    +auth fixed +flyway plugin added +DB migration script added

--- src/main/java/ru/geekbrains/springbootdemo/entities/Role.java
+++ src/main/java/ru/geekbrains/springbootdemo/entities/Role.java
@@ -3,6 +3,7 @@ package ru.geekbrains.springbootdemo.entities;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
@@ -16,10 +17,7 @@ public class Role {
     @Column(name = "name")
     private String name;
 
-    public Role() {
-    }
-
-    public Role(String name) {
-        this.name = name;
-    }
+    @ManyToMany(mappedBy = "roles",
+            fetch = FetchType.LAZY)
+    private List<User> users;
 }
